4cf61f0e145bac5d1002c29526d1f763d88bae68
guilherme jose <jose@senac.pe>
2022-08-10 16:45:54 -0300

    arquivo operações

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log(teste)
+const op = require('./operacoes.js')
+
+console.log(op.somar(2,5))
+console.log(op.subtrair(5,2))
---
+++ operacoes.js
@@ -0,0 +1,10 @@
+function somar(a,b){
+return a + b
+}
+
+
+function subtrair(a,b){
+    return a - b
+    }
+    
+module.exports = {somar , subtrair}
